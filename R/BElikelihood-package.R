9abc7fb289674f30c7f5345ab4e0d8856cdcd408
beckca <cole.beck@vanderbilt.edu>
2024-02-28 09:37:32 -0600

    add LC doc

--- R/BElikelihood-package.R
+++ R/BElikelihood-package.R
@@ -1,7 +1,8 @@
-#' Likelihood method for analyzing Bioequivalence
+#' Likelihood method for analyzing bioequivalence (BE) trial data
 #'
-#' This package will calculate and plot the profile likelihoods for the mean difference and standard deviation ratios of a test drug to a reference drug for
-#' AUC or Cmax from a full replicate 2x4 (RTRT/TRTR) cross-over, 2x3 partial replicate (TRT/RTR) cross-over, or a 2x2 (RT/TR) cross-over design (where R and T for reference and test drugs, respectively). 
+#' This package will calculate and plot the profile likelihoods for the mean difference and standard deviation ratios of a test drug 
+#' to a reference drug for AUC or Cmax from various crossover designs commonly used in BE studies, such as a fully replicated crossover 
+#' design (e.g., 2x4 two-sequence, four-period, RTRT/TRTR), a partially replicated crossover design (e.g., 2x3, two-sequence, three-period, RTR/TRT), and a two-sequence, two-period, crossover design design (2x2, RT/TR), where "R" stands for a reference drug and "T" stands for a test drug.
 #'
 #' @docType package
 #'
--- R/dat.R
+++ R/dat.R
@@ -1,24 +1,24 @@
-#' A typical bioequivalence (BE) study data
+#' Data example for bioequivalence (BE) study
 #'
-#' The dataset is a typical bioequivalence data from a full repicate 2x4 cross-over design with RTRT and TRTR as sequences. 
-#' It is a subset of Example 4.4 in Chapter 4 of Patterson and Jones's book with some modifications and missing data.
+#' The dataset is a bioequivalence dataset from a fully repicated 2x4 crossover design with RTRT and TRTR as sequences. 
+#' It is a subset of Example 4.4 in Chapter 4 of Patterson and Jones's book.
 #'
 #' @format
-#' A data frame with 176 observations (for 44 subjects) on 6 variables:
+#' A data frame with 176 observations (from 44 subjects) on 6 variables:
 #' \describe{
 #'   \item{subject}{subject ID}
-#'   \item{sequence}{RTRT or TRTR, T and R for test and reference drugs, respectively}
-#'   \item{period}{1 to 4 for cross-over period}
-#'   \item{formula}{T or R for test and reference drug names}
-#'   \item{AUC}{a pharmacokinetic parameter-the area under the blood/plasma concentration-time curve}
-#'   \item{CMAX}{a pharmacokinetic parameter-the peak concentration}
+#'   \item{sequence}{RTRT or TRTR, where T and R stand for test and reference drugs, respectively}
+#'   \item{period}{1 to 4 for crossover period}
+#'   \item{formula}{T or R stand for test and reference drugs, respectively}
+#'   \item{AUC}{a pharmacokinetic parameter - the area under the blood/plasma concentration-time curve}
+#'   \item{CMAX}{a pharmacokinetic parameter - the peak concentration}
 #' }
 #'
 #' @usage data(dat, package = 'BElikelihood')
 #'
 #' @keywords dataset
 #'
-#' @source Patterson S and Jones B, Bioequivalence and Statistics in Clinical Pharmacology. Chapman Hall/CRC press: Boca London and New York, 2005
+#' @source Patterson S and Jones B (2023). Bioequivalence and Statistics in Clinical Pharmacology. Chapman Hall/CRC Press.
 #' 
 #' @examples
 #' data(dat)
